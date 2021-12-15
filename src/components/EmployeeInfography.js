ddf513244066f9c1ae509daa657bbb70d636f229
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2021-12-15 10:21:29 +0100

    chore: add picto employee

--- src/components/EmployeeInfography.js
+++ src/components/EmployeeInfography.js
@@ -2,49 +2,54 @@ import React from "react"
 
 const EmployeeInfography = () => {
   return (
-    <div className="fr-container infography">
+    <div className="fr-container infography infography__employee">
+      <h2>{`Transitions collectives c'est`}</h2>
       <div className="fr-grid-row block">
         <div className="fr-col-12">
-          <div className="fr-grid-row fr-grid-row--center fr-grid-row--gutters">
-            <div className="fr-col-md-4 fr-col-12 fr-p-5w">
-              {/* <h3 className="emphasis fr-mb-3w fr-h6">
-                <strong>Une action responsable</strong>
-              </h3> */}
+          <div className="fr-grid-row fr-grid-row--gutters">
+            <div className="fr-col-md-4 fr-col-12 fr-p-4w">
+              <img
+                alt=""
+                src="/images/employee/picto_1.png"
+                aria-hidden="true"
+              />
               <p>
                 {`Une formation vers un métier d’avenir et qui recrute près de chez vous.`}
               </p>
             </div>
-            <div className="fr-col-md-4 fr-col-12 fr-p-5w">
-              {/* <h3 className="emphasis fr-mb-3w fr-h6">
-                <strong>Un accompagnement des salariés</strong>
-              </h3> */}
+            <div className="fr-col-md-4 fr-col-12 fr-p-4w">
+              <img
+                alt=""
+                src="/images/employee/picto_2.png"
+                aria-hidden="true"
+              />
               <p>{`Un accompagnement personnalisé pour vous reconvertir.`}</p>
             </div>
 
-            <div className="fr-col-md-4 fr-col-12 fr-p-5w">
-              {/* <h3 className="emphasis fr-mb-3w fr-h6">
-                <strong>
-                  Une démarche de maintien dans l’emploi au niveau local
-                </strong>
-              </h3> */}
+            <div className="fr-col-md-4 fr-col-12 fr-p-4w">
+              <img
+                alt=""
+                src="/images/employee/picto_3.png"
+                aria-hidden="true"
+              />
               <p>{`Le financement complet de votre parcours de formation.`}</p>
             </div>
-            <div className="fr-col-md-4 fr-col-12 fr-p-5w">
-              {/* <h3 className="emphasis fr-mb-3w fr-h6">
-                <strong>
-                  Une démarche de maintien dans l’emploi au niveau local
-                </strong>
-              </h3> */}
+            <div className="fr-col-md-4 fr-col-12 fr-p-4w">
+              <img
+                alt=""
+                src="/images/employee/picto_4.png"
+                aria-hidden="true"
+              />
               <p>
                 {`Le maintien d’un niveau important de rémunération pendant toute la durée de la formation.`}
               </p>
             </div>
-            <div className="fr-col-md-4 fr-col-12 fr-p-5w">
-              {/* <h3 className="emphasis fr-mb-3w fr-h6">
-                <strong>
-                  Une démarche de maintien dans l’emploi au niveau local
-                </strong>
-              </h3> */}
+            <div className="fr-col-md-4 fr-col-12 fr-p-4w">
+              <img
+                alt=""
+                src="/images/employee/picto_5.png"
+                aria-hidden="true"
+              />
               <p>
                 {`La sécurité de l’emploi par le maintien de votre contrat de travail.`}
               </p>
--- src/pages/je-suis-salarie.js
+++ src/pages/je-suis-salarie.js
@@ -8,13 +8,20 @@ import Layout from "@/components/Layout"
 const Page = () => {
   return (
     <Layout>
-      <section className="fr-py-4w" id="infography">
+      <div className="fr-container">
+        <div className="fr-grid-row">
+          <div className="fr-col-12 fr-col-lg-6 fr-mt-4w">
+            <h1>Je suis un salarié</h1>
+          </div>
+        </div>
+      </div>
+      <section className="fr-py-2w" id="infography">
         <EmployeeInfography />
       </section>
-      <section className="fr-py-4w" id="video">
+      <section className="fr-py-2w" id="video">
         <EmployeeVideo />
       </section>
-      <section className="fr-py-4w" id="faq">
+      <section className="fr-py-2w" id="faq">
         <EmployeeFaq />
       </section>
     </Layout>
