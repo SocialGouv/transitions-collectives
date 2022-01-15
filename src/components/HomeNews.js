35334d4a4b2b10b51ee1c48121444f25fdfb7471
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2022-01-15 21:24:38 +0100

    feat(home): put news on one line

--- src/components/HomeNews.js
+++ src/components/HomeNews.js
@@ -22,28 +22,28 @@ const HomeNews = () => {
     <div className="fr-container home-news">
       <h2 className="fr-mb-6w">{`Actualités`} </h2>
       <div className="fr-grid-row fr-grid-row--gutters">
-        <div className="fr-col-12 fr-col-md-6 fr-col-lg-4">
+        <div className="fr-col-12 fr-col-md-6 fr-col-lg-3">
           <NewsCard
             image="images/news/mobilisation-manpower-et-adecco.jpg"
             title="Mobilisation de ManpowerGroup France et Adecco , leaders de l’intérim"
             link="/actualites/mobilisation-manpower-et-adecco"
           />
         </div>
-        <div className="fr-col-12 fr-col-md-6 fr-col-lg-4">
+        <div className="fr-col-12 fr-col-md-6 fr-col-lg-3">
           <NewsCard
             image="images/news/retour-premier-projet-transco.jpg"
             title="Retour sur le premier projet de Transitions collectives"
             link="/actualites/retour-premier-projet-transco"
           />
         </div>
-        <div className="fr-col-12 fr-col-md-6 fr-col-lg-4">
+        <div className="fr-col-12 fr-col-md-6 fr-col-lg-3">
           <NewsCard
             image="images/news/sifa-technologies.jpg"
             title="Julien Barbrault et Stéphanie Roux, directeur et RRH de l’entreprise SIFA Technologies"
             link="/actualites/sifa-technologies"
           />
         </div>
-        <div className="fr-col-12 fr-col-md-6 fr-col-lg-4">
+        <div className="fr-col-12 fr-col-md-6 fr-col-lg-3">
           <NewsCard
             image="images/news/formation-pour-devenir-conducteur.png"
             title="Farid Mechhat, en formation pour devenir conducteur de véhicules"
