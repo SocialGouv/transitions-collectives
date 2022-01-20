6677676c8c9546eaa1aeba564e88d6895b6f812e
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2022-01-20 10:03:14 +0100

    feat: implement search contact

---
+++ src/data/departements.js
@@ -0,0 +1,507 @@
+export default [
+  {
+    label: "01 - Ain",
+    region: "Auvergne-Rhône-Alpes",
+    value: "01",
+  },
+  {
+    label: "02 - Aisne",
+    region: "Hauts-de-France",
+    value: "02",
+  },
+  {
+    label: "03 - Allier",
+    region: "Auvergne-Rhône-Alpes",
+    value: "03",
+  },
+  {
+    label: "04 - Alpes-de-Haute-Provence",
+    region: "Provence-Alpes-Côte d'Azur",
+    value: "04",
+  },
+  {
+    label: "05 - Hautes-Alpes",
+    region: "Provence-Alpes-Côte d'Azur",
+    value: "05",
+  },
+  {
+    label: "06 - Alpes-Maritimes",
+    region: "Provence-Alpes-Côte d'Azur",
+    value: "06",
+  },
+  {
+    label: "07 - Ardèche",
+    region: "Auvergne-Rhône-Alpes",
+    value: "07",
+  },
+  {
+    label: "08 - Ardennes",
+    region: "Grand Est",
+    value: "08",
+  },
+  {
+    label: "09 - Ariège",
+    region: "Occitanie",
+    value: "09",
+  },
+  {
+    label: "10 - Aube",
+    region: "Grand Est",
+    value: "10",
+  },
+  {
+    label: "11 - Aude",
+    region: "Occitanie",
+    value: "11",
+  },
+  {
+    label: "12 - Aveyron",
+    region: "Occitanie",
+    value: "12",
+  },
+  {
+    label: "13 - Bouches-du-Rhône",
+    region: "Provence-Alpes-Côte d'Azur",
+    value: "13",
+  },
+  {
+    label: "14 - Calvados",
+    region: "Normandie",
+    value: "14",
+  },
+  {
+    label: "15 - Cantal",
+    region: "Auvergne-Rhône-Alpes",
+    value: "15",
+  },
+  {
+    label: "16 - Charente",
+    region: "Nouvelle-Aquitaine",
+    value: "16",
+  },
+  {
+    label: "17 - Charente-Maritime",
+    region: "Nouvelle-Aquitaine",
+    value: "17",
+  },
+  {
+    label: "18 - Cher",
+    region: "Centre-Val de Loire",
+    value: "18",
+  },
+  {
+    label: "19 - Corrèze",
+    region: "Nouvelle-Aquitaine",
+    value: "19",
+  },
+  {
+    label: "21 - Côte-d'Or",
+    region: "Bourgogne-Franche-Comté",
+    value: "21",
+  },
+  {
+    label: "22 - Côtes-d'Armor",
+    region: "Bretagne",
+    value: "22",
+  },
+  {
+    label: "23 - Creuse",
+    region: "Nouvelle-Aquitaine",
+    value: "23",
+  },
+  {
+    label: "24 - Dordogne",
+    region: "Nouvelle-Aquitaine",
+    value: "24",
+  },
+  {
+    label: "25 - Doubs",
+    region: "Bourgogne-Franche-Comté",
+    value: "25",
+  },
+  {
+    label: "26 - Drôme",
+    region: "Auvergne-Rhône-Alpes",
+    value: "26",
+  },
+  {
+    label: "27 - Eure",
+    region: "Normandie",
+    value: "27",
+  },
+  {
+    label: "28 - Eure-et-Loir",
+    region: "Centre-Val de Loire",
+    value: "28",
+  },
+  {
+    label: "29 - Finistère",
+    region: "Bretagne",
+    value: "29",
+  },
+  {
+    label: "2A - Corse-du-Sud",
+    region: "Corse",
+    value: "2A",
+  },
+  {
+    label: "2B - Haute-Corse",
+    region: "Corse",
+    value: "2B",
+  },
+  {
+    label: "30 - Gard",
+    region: "Occitanie",
+    value: "30",
+  },
+  {
+    label: "31 - Haute-Garonne",
+    region: "Occitanie",
+    value: "31",
+  },
+  {
+    label: "32 - Gers",
+    region: "Occitanie",
+    value: "32",
+  },
+  {
+    label: "33 - Gironde",
+    region: "Nouvelle-Aquitaine",
+    value: "33",
+  },
+  {
+    label: "34 - Hérault",
+    region: "Occitanie",
+    value: "34",
+  },
+  {
+    label: "35 - Ille-et-Vilaine",
+    region: "Bretagne",
+    value: "35",
+  },
+  {
+    label: "36 - Indre",
+    region: "Centre-Val de Loire",
+    value: "36",
+  },
+  {
+    label: "37 - Indre-et-Loire",
+    region: "Centre-Val de Loire",
+    value: "37",
+  },
+  {
+    label: "38 - Isère",
+    region: "Auvergne-Rhône-Alpes",
+    value: "38",
+  },
+  {
+    label: "39 - Jura",
+    region: "Bourgogne-Franche-Comté",
+    value: "39",
+  },
+  {
+    label: "40 - Landes",
+    region: "Nouvelle-Aquitaine",
+    value: "40",
+  },
+  {
+    label: "41 - Loir-et-Cher",
+    region: "Centre-Val de Loire",
+    value: "41",
+  },
+  {
+    label: "42 - Loire",
+    region: "Auvergne-Rhône-Alpes",
+    value: "42",
+  },
+  {
+    label: "43 - Haute-Loire",
+    region: "Auvergne-Rhône-Alpes",
+    value: "43",
+  },
+  {
+    label: "44 - Loire-Atlantique",
+    region: "Pays de la Loire",
+    value: "44",
+  },
+  {
+    label: "45 - Loiret",
+    region: "Centre-Val de Loire",
+    value: "45",
+  },
+  {
+    label: "46 - Lot",
+    region: "Occitanie",
+    value: "46",
+  },
+  {
+    label: "47 - Lot-et-Garonne",
+    region: "Nouvelle-Aquitaine",
+    value: "47",
+  },
+  {
+    label: "48 - Lozère",
+    region: "Occitanie",
+    value: "48",
+  },
+  {
+    label: "49 - Maine-et-Loire",
+    region: "Pays de la Loire",
+    value: "49",
+  },
+  {
+    label: "50 - Manche",
+    region: "Normandie",
+    value: "50",
+  },
+  {
+    label: "51 - Marne",
+    region: "Grand Est",
+    value: "51",
+  },
+  {
+    label: "52 - Haute-Marne",
+    region: "Grand Est",
+    value: "52",
+  },
+  {
+    label: "53 - Mayenne",
+    region: "Pays de la Loire",
+    value: "53",
+  },
+  {
+    label: "54 - Meurthe-et-Moselle",
+    region: "Grand Est",
+    value: "54",
+  },
+  {
+    label: "55 - Meuse",
+    region: "Grand Est",
+    value: "55",
+  },
+  {
+    label: "56 - Morbihan",
+    region: "Bretagne",
+    value: "56",
+  },
+  {
+    label: "57 - Moselle",
+    region: "Grand Est",
+    value: "57",
+  },
+  {
+    label: "58 - Nièvre",
+    region: "Bourgogne-Franche-Comté",
+    value: "58",
+  },
+  {
+    label: "59 - Nord",
+    region: "Hauts-de-France",
+    value: "59",
+  },
+  {
+    label: "60 - Oise",
+    region: "Hauts-de-France",
+    value: "60",
+  },
+  {
+    label: "61 - Orne",
+    region: "Normandie",
+    value: "61",
+  },
+  {
+    label: "62 - Pas-de-Calais",
+    region: "Hauts-de-France",
+    value: "62",
+  },
+  {
+    label: "63 - Puy-de-Dôme",
+    region: "Auvergne-Rhône-Alpes",
+    value: "63",
+  },
+  {
+    label: "64 - Pyrénées-Atlantiques",
+    region: "Nouvelle-Aquitaine",
+    value: "64",
+  },
+  {
+    label: "65 - Hautes-Pyrénées",
+    region: "Occitanie",
+    value: "65",
+  },
+  {
+    label: "66 - Pyrénées-Orientales",
+    region: "Occitanie",
+    value: "66",
+  },
+  {
+    label: "67 - Bas-Rhin",
+    region: "Grand Est",
+    value: "67",
+  },
+  {
+    label: "68 - Haut-Rhin",
+    region: "Grand Est",
+    value: "68",
+  },
+  {
+    label: "69 - Rhône",
+    region: "Auvergne-Rhône-Alpes",
+    value: "69",
+  },
+  {
+    label: "70 - Haute-Saône",
+    region: "Bourgogne-Franche-Comté",
+    value: "70",
+  },
+  {
+    label: "71 - Saône-et-Loire",
+    region: "Bourgogne-Franche-Comté",
+    value: "71",
+  },
+  {
+    label: "72 - Sarthe",
+    region: "Pays de la Loire",
+    value: "72",
+  },
+  {
+    label: "73 - Savoie",
+    region: "Auvergne-Rhône-Alpes",
+    value: "73",
+  },
+  {
+    label: "74 - Haute-Savoie",
+    region: "Auvergne-Rhône-Alpes",
+    value: "74",
+  },
+  {
+    label: "75 - Paris",
+    region: "Île-de-France",
+    value: "75",
+  },
+  {
+    label: "76 - Seine-Maritime",
+    region: "Normandie",
+    value: "76",
+  },
+  {
+    label: "77 - Seine-et-Marne",
+    region: "Île-de-France",
+    value: "77",
+  },
+  {
+    label: "78 - Yvelines",
+    region: "Île-de-France",
+    value: "78",
+  },
+  {
+    label: "79 - Deux-Sèvres",
+    region: "Nouvelle-Aquitaine",
+    value: "79",
+  },
+  {
+    label: "80 - Somme",
+    region: "Hauts-de-France",
+    value: "80",
+  },
+  {
+    label: "81 - Tarn",
+    region: "Occitanie",
+    value: "81",
+  },
+  {
+    label: "82 - Tarn-et-Garonne",
+    region: "Occitanie",
+    value: "82",
+  },
+  {
+    label: "83 - Var",
+    region: "Provence-Alpes-Côte d'Azur",
+    value: "83",
+  },
+  {
+    label: "84 - Vaucluse",
+    region: "Provence-Alpes-Côte d'Azur",
+    value: "84",
+  },
+  {
+    label: "85 - Vendée",
+    region: "Pays de la Loire",
+    value: "85",
+  },
+  {
+    label: "86 - Vienne",
+    region: "Nouvelle-Aquitaine",
+    value: "86",
+  },
+  {
+    label: "87 - Haute-Vienne",
+    region: "Nouvelle-Aquitaine",
+    value: "87",
+  },
+  {
+    label: "88 - Vosges",
+    region: "Grand Est",
+    value: "88",
+  },
+  {
+    label: "89 - Yonne",
+    region: "Bourgogne-Franche-Comté",
+    value: "89",
+  },
+  {
+    label: "90 - Territoire de Belfort",
+    region: "Bourgogne-Franche-Comté",
+    value: "90",
+  },
+  {
+    label: "91 - Essonne",
+    region: "Île-de-France",
+    value: "91",
+  },
+  {
+    label: "92 - Hauts-de-Seine",
+    region: "Île-de-France",
+    value: "92",
+  },
+  {
+    label: "93 - Seine-Saint-Denis",
+    region: "Île-de-France",
+    value: "93",
+  },
+  {
+    label: "94 - Val-de-Marne",
+    region: "Île-de-France",
+    value: "94",
+  },
+  {
+    label: "95 - Val-d'Oise",
+    region: "Île-de-France",
+    value: "95",
+  },
+  {
+    label: "971 - Guadeloupe",
+    region: "Guadeloupe",
+    value: "971",
+  },
+  {
+    label: "972 - Martinique",
+    region: "Martinique",
+    value: "972",
+  },
+  {
+    label: "973 - Guyane",
+    region: "Guyane",
+    value: "973",
+  },
+  {
+    label: "974 - La Réunion",
+    region: "La Réunion",
+    value: "974",
+  },
+  {
+    label: "976 - Mayotte",
+    region: "Mayotte",
+    value: "976",
+  },
+]
