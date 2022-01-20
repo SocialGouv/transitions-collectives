db2504328952fa7e39c8696451f2dc2848b947af
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2022-01-20 15:04:06 +0100

    fix: build

--- src/pages/sites-utiles.js
+++ src/pages/sites-utiles.js
@@ -1,8 +1,8 @@
 import Head from "next/head"
+import Link from "next/link"
 import React from "react"
 
 import Layout from "@/components/Layout"
-import Link from "next/link"
 
 const Page = () => {
   return (
