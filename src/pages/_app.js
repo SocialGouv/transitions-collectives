799974f80a877be09e6c2efe6a4858da38394492
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2022-02-04 12:33:55 +0100

    fix: matomo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,6 +10,12 @@ Sentry.init({
   dsn: process.env.SENTRY_DSN,
 })
 
+console.log({
+  dsn: process.env.SENTRY_DSN,
+  matomoSiteId: process.env.MATOMO_SITE_ID,
+  matomoUrl: process.env.MATOMO_URL,
+})
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
