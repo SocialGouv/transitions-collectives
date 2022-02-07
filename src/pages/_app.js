cc23db4c05c92e6d99a2ac93b62cfa56a0ca2391
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2022-02-07 10:22:48 +0100

    fix: matomo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,24 +6,15 @@ import { init } from "@socialgouv/matomo-next"
 import App from "next/app"
 import React from "react"
 
-const MATOMO_URL = process.env.MATOMO_URL
-const MATOMO_SITE_ID = process.env.MATOMO_SITE_ID
-
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 })
 
-console.log({
-  matomoSiteId: process.env.MATOMO_SITE_ID,
-  matomoUrl: process.env.MATOMO_URL,
-})
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID
 
 class MyApp extends App {
   componentDidMount() {
-    console.log({
-      matomoSiteId: process.env.MATOMO_SITE_ID,
-      matomoUrl: process.env.MATOMO_URL,
-    })
     init({ siteId: MATOMO_SITE_ID, url: MATOMO_URL })
   }
   render() {
