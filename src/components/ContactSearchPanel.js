280e9fd0d6ac3b47ad17ff1e39b27c98579902fe
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2022-02-15 11:47:09 +0100

    feat: contact info (#61)

--- src/components/ContactSearchPanel.js
+++ src/components/ContactSearchPanel.js
@@ -19,70 +19,156 @@ const options = {
 }
 const fuse = new Fuse(contacts, options)
 
-const StructureCard = ({ structure }) => (
-  <div className="card fr-p-3w">
-    <p className="fr-text--lg fr-mb-1w">
-      <strong>
-        {structure.name} {structure.opcoNetwork || ""}
-      </strong>
-      <br />
-    </p>
-    <p className="fr-mb-2w">{structure.address}</p>
-    {structure.email && (
-      <div className="fr-mb-2w">
-        <div className="icon-link">
-          <i className="ri-mail-fill" aria-hidden="true" />
-          <a
-            rel="noopener noreferrer"
-            title={`Envoyer un email à ${structure.email} - ouvre une nouvelle fenêtre`}
-            href={`mailto:${structure.email}`}
-          >
-            {structure.email}
-          </a>
-        </div>
-      </div>
-    )}
-    {!structure.email &&
-      structure.contacts?.map((contact, index) => {
-        return (
-          <div key={index} className="fr-mb-2w">
-            {contact.role && (
-              <p className="fr-text--md fr-mb-1w">
-                <strong>{contact.role}</strong>
-                <br />
-              </p>
-            )}
-            <div className="icon-link">
-              <i className="ri-mail-fill" aria-hidden="true" />
-              <a
-                rel="noopener noreferrer"
-                title={`Envoyer un email à ${contact.email} - ouvre une nouvelle fenêtre`}
-                href={`mailto:${contact.email}`}
-              >
-                {contact.email}
-              </a>
-            </div>
-          </div>
-        )
-      })}
-    {structure.website && (
-      <div className="fr-mb-2w">
-        <div className="icon-link">
-          <i className="ri-global-line" aria-hidden="true" />
-          <a
-            rel="noopener noreferrer"
-            target="_blank"
-            title={`Site web`}
-            href={structure.website}
-          >
-            {structure.website}
-          </a>
-        </div>
-      </div>
-    )}
-    <p className="fr-text--sm fr-mb-2w">{structure.comment}</p>
-  </div>
+const DarpInfo = () => (
+  <section className="fr-text--sm">
+    {
+      "Le délégué à l'accompagnement des reconversions professionnelles (DARP) est votre interlocuteur privilégié sur le territoire. Il vous accompagne pour :"
+    }
+    <ul>
+      <li>
+        - repérer les enjeux RH de votre entreprise, vos besoins notamment en
+        matière de compétences, et identifier avec vous des solutions
+      </li>
+      <li>
+        - faciliter l’identification et le soutien d’interlocuteurs de proximité
+        (DDETS, Pôle emploi, OPCO, organismes de formation…)
+      </li>
+      <li>
+        - échanger et suivre la démarche de Transitions collectives de votre
+        entreprise
+      </li>
+    </ul>
+  </section>
 )
+
+const OpcoInfo = () => (
+  <section className="fr-text--sm">
+    Votre opérateur de compétences (OPCO) :
+    <ul>
+      <li>- pour vous accompagner dans la réalisation d’un diagnostic RH</li>{" "}
+      <li>
+        - pour identifier les métiers fragilisés au sein de votre entreprise
+      </li>{" "}
+      <li>
+        - pour un appui à la réalisation de votre dossier de demande de
+        Transitions collectives
+      </li>
+    </ul>
+  </section>
+)
+
+const AtProInfo = () => (
+  <section className="fr-text--sm">
+    Votre association de Transitions pro (ATpro) :
+    <ul>
+      <li>- pour constituer les dossiers de demande de vos salariés</li>
+    </ul>
+  </section>
+)
+
+const hasInfo = (type) => {
+  return ["DARP", "At Pro", "OPCO"].includes(type)
+}
+
+const StructureCard = ({ structure }) => {
+  const [showInfo, setShowInfo] = useState(false)
+
+  return (
+    <div className="search-contact__card fr-p-3w">
+      <p className="fr-text--lg fr-mb-1w">
+        <strong>
+          {structure.name} {structure.opcoNetwork || ""}
+        </strong>
+        {hasInfo(structure.name) && (
+          <button type="button" onClick={() => setShowInfo(structure.name)}>
+            <i className="ri-question-line fr-text--lg" aria-hidden="true" />
+          </button>
+        )}
+        <br />
+      </p>
+      <p className="fr-mb-2w">{structure.address}</p>
+      {structure.email && (
+        <div className="fr-mb-2w">
+          <div className="icon-link">
+            <i className="ri-mail-fill" aria-hidden="true" />
+            <a
+              rel="noopener noreferrer"
+              title={`Envoyer un email à ${structure.email} - ouvre une nouvelle fenêtre`}
+              href={`mailto:${structure.email}`}
+            >
+              {structure.email}
+            </a>
+          </div>
+        </div>
+      )}
+      {!structure.email &&
+        structure.contacts?.map((contact, index) => {
+          return (
+            <div key={index} className="fr-mb-2w">
+              {contact.role && (
+                <p className="fr-text--lg fr-mb-1w">
+                  <strong>{contact.role}</strong>
+                  <button
+                    type="button"
+                    onClick={() => setShowInfo(contact.role)}
+                  >
+                    <i
+                      className="ri-question-line fr-text--lg"
+                      aria-hidden="true"
+                    />
+                  </button>
+                  <br />
+                </p>
+              )}
+              <div className="icon-link">
+                <i className="ri-mail-fill" aria-hidden="true" />
+                <a
+                  rel="noopener noreferrer"
+                  title={`Envoyer un email à ${contact.email} - ouvre une nouvelle fenêtre`}
+                  href={`mailto:${contact.email}`}
+                >
+                  {contact.email}
+                </a>
+              </div>
+            </div>
+          )
+        })}
+      {structure.website && (
+        <div className="fr-mb-2w">
+          <div className="icon-link">
+            <i className="ri-global-line" aria-hidden="true" />
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              title={`Site web`}
+              href={structure.website}
+            >
+              {structure.website}
+            </a>
+          </div>
+        </div>
+      )}
+      <p className="fr-text--sm fr-mb-2w">{structure.comment}</p>
+      {showInfo && (
+        <div className="search-contact__info fr-p-3w">
+          <section className="search-contact__info__top fr-pb-2w">
+            <strong>{showInfo}</strong>
+            <button
+              type="button"
+              onClick={() => setShowInfo(false)}
+              aria-label="Fermer"
+            >
+              <i className="ri-close-circle-line fr-text--md" />
+            </button>
+          </section>
+          {showInfo === "DARP" && <DarpInfo />}
+          {showInfo === "OPCO" && <OpcoInfo />}
+          {showInfo === "At Pro" && <AtProInfo />}
+        </div>
+      )}
+    </div>
+  )
+}
 
 const ContactSearchPanel = () => {
   const [selectedDepartment, setSelectedDepartment] = useState("")
@@ -197,7 +283,7 @@ const ContactSearchPanel = () => {
   }
 
   return (
-    <section className="search-contacts fr-py-3w background-grey">
+    <section className="search-contact fr-py-3w">
       <div className="fr-container">
         <h2 className="fr-text--md fr-text--regular">
           Prenez contact avec vos correspondants pour toute information
