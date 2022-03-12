ccd85bf6bec1d3a092832660072e801b4eff7a8b
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2022-03-12 18:00:04 +0100

    fix: add question to faq

--- src/components/EmployeeFaq/index.js
+++ src/components/EmployeeFaq/index.js
@@ -1,3 +1,4 @@
+import { Accordion, AccordionItem } from "@dataesr/react-dsfr"
 import React from "react"
 
 import EmployeeFaqApresFormation from "./EmployeeFaqApresFormation"
@@ -9,6 +10,33 @@ const EmployeeFaq = () => {
   return (
     <div className="fr-container fr-py-4w faq">
       <h2 className="fr-h3">{`Je me pose des questions`}</h2>
+      <div className="fr-py-2w">
+        <Accordion>
+          <AccordionItem title="Comment puis-avoir accès aux Transitions collectives ?">
+            <p>
+              Vous avez la possibilité d’entrer volontairement dans un parcours
+              de Transitions collectives dans les deux cas suivants :
+              <ul>
+                <li>
+                  <u>
+                    Si votre entreprise a identifié votre métier comme étant
+                    fragilisé
+                  </u>{" "}
+                  au sein d’un accord de gestion des emplois et des parcours
+                  professionnels (GEPP) ;{" "}
+                </li>
+                <li>
+                  <u> Dans le cadre d’un congé de mobilité</u>, si votre
+                  entreprise a signé un accord GEPP ou une rupture
+                  conventionnelle collective (RCC) prévoyant le recours au congé
+                  de mobilité et autorisant les salariés à bénéficier au
+                  dispositif Transitions collectives.
+                </li>
+              </ul>
+            </p>
+          </AccordionItem>
+        </Accordion>
+      </div>
       <div className="fr-py-2w">
         <EmployeeFaqAvantFormation />
       </div>
